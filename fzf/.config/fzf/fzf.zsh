92c9df97bc75f06c9f46d0fd5ca4a483d2d79f30
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2021-08-25 17:23:34 -0300

    fzf: add configuration

---
+++ fzf/.config/fzf/fzf.zsh
@@ -0,0 +1,14 @@
+# Setup fzf
+# ---------
+# if [[ ! "$PATH" == */opt/fzf/bin* ]]; then
+#   export PATH="${PATH:+${PATH}:}/opt/fzf/bin"
+# fi
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh" 2> /dev/null
+
+# Key bindings
+# ------------
+[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.zsh" 2> /dev/null
+# source "/opt/fzf/shell/key-bindings.zsh"
