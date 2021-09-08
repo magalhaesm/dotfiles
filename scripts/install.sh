f17b0fafdb5038008df2b8273c785a1d7f5a6f8b
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2021-09-08 02:51:35 -0300

    zsh: update install script

--- scripts/install.sh
+++ scripts/install.sh
@@ -8,6 +8,14 @@ GREEN="\033[1;32m"
 DOTFILES="$HOME/.dotfiles"
 USER_ENVS="\$HOME/.config/zsh/exports.zsh"
 
+dependencies=(
+  stow kitty tmux delta
+  nvim gcc node
+  zsh rg bat zoxide fzf exa fd starship
+)
+
+missing=()
+
 info() {
   echo -e "\n${CYAN}info:${NC} $1"
 }
@@ -23,9 +31,55 @@ installed() {
   fi
 }
 
+check_sys_deps() {
+  if ! installed git || ! installed curl
+  then
+    echo -e "\nPlease, install and try again."
+    exit 1
+  fi
+}
+
 clone_dotfiles() {
   [ -d "$DOTFILES" ] && { echo "Directory $DOTFILES already exists." && return; }
   git clone https://github.com/magalhaesm/dotfiles.git "$DOTFILES"
+}
+
+check_deps() {
+  for dep in "${dependencies[@]}"
+  do
+    ! installed "$dep" && missing+=("$dep")
+  done
+}
+
+install_missing() {
+  if [ "${#missing[@]}" != 0 ]
+  then
+    read -p " Install missing dependencies? [y/N] " -r install
+    [ "$install" != "y" ] && exit
+
+    info "Installing dependencies..."
+    for tool in "${missing[@]}"
+    do
+      echo -e " ==> ${GREEN}${tool}${NC}"
+      "$DOTFILES/scripts/install-$tool"
+    done
+  fi
+}
+
+config() {
+  [ -d "$1" ] && stow -vt "$HOME" "$1"
+}
+
+stow_configs() {
+  pushd "$DOTFILES" &>/dev/null || exit 1
+
+  config git
+  for tool in "${dependencies[@]}"
+  do
+    config "$tool"
+  done
+
+  popd &>/dev/null || exit 1
 }
 
 set_zshenv() {
@@ -34,79 +88,39 @@ set_zshenv() {
     echo "Editing /etc/zsh/zshenv"
 
     echo -e "\n# Load user envs" | sudo tee -a /etc/zsh/zshenv
-    echo -e "[ -f $USER_ENVS ] && source $USER_ENVS" | sudo tee -a /etc/zsh/zshenv
+    echo -e "[ -f \"$USER_ENVS\" ] && source \"$USER_ENVS\"" | sudo tee -a /etc/zsh/zshenv
     info "Restart the session to take effect."
   fi
 }
 
 set_shell() {
   zsh=$(command -v zsh)
-  if [ "$SHELL" != "$zsh" ]
-  then
-    chsh -s "$zsh" "$USER"
-  else
-    echo "Already configured."
-  fi
+  [ "$SHELL" != "$zsh" ] && chsh -s "$zsh" "$USER" || echo "Already configured."
 }
 
-config() {
-  [ -d "$1" ] && stow -vt "$HOME" "$1"
+main() {
+  echo "---------------------------------------------------------"
+  echo " ==> Installing terminal, tools and configurations "
+  echo "---------------------------------------------------------"
+
+  info "Checking installation dependencies..."
+  check_sys_deps
+
+  info "Obtaining repository..."
+  clone_dotfiles
+
+  info "Checking dependencies..."
+  check_deps
+  install_missing
+
+  info "Stowing configuration files..."
+  stow_configs
+
+  info "Ensuring user environment variables..."
+  set_zshenv
+
+  info "Setting zsh as default shell..."
+  set_shell
 }
 
-echo "---------------------------------------------------------"
-echo " ==> Installing terminal, tools and configurations "
-echo "---------------------------------------------------------"
-
-info "Checking main dependencies..."
-if ! installed git || ! installed curl
-then
-  echo -e "\nPlease, install and try again."
-  exit 1
-fi
-
-info "Obtaining repository..."
-clone_dotfiles
-
-dependencies=(
-  stow kitty tmux delta
-  nvim gcc node
-  zsh rg bat zoxide fzf exa fd starship
-)
-
-missing=()
-
-info "Checking dependencies..."
-for dep in "${dependencies[@]}"
-do
-  ! installed "$dep" && missing+=("$dep")
-done
-
-if [ "${#missing[@]}" != 0 ]
-then
-  read -p " Install missing dependencies? [y/N] " -r install
-  [ "$install" != "y" ] && exit
-
-  info "Installing dependencies..."
-  for tool in "${missing[@]}"
-  do
-    echo -e " ==> ${GREEN}$tool${NC}"
-    "$DOTFILES/scripts/install-$tool"
-  done
-fi
-
-info "Stowing configuration files..."
-pushd "$DOTFILES" &>/dev/null || exit 1
-
-config git
-for tool in "${dependencies[@]}"
-do
-  config "$tool"
-done
-
-popd &>/dev/null || exit 1
-
-info "Ensuring user environment variables..."
-set_zshenv
-
-info "Setting zsh as default shell..."
-set_shell
+main
