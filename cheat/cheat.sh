be987b2b89792e332073104bb203ec7c59a9f64a
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2021-09-15 12:09:36 -0300

    tmux: add cheat.sh as keybinding

---
+++ cheat/cheat.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+list() {
+  arr=("$@")
+  echo "${arr[*]}" | tr ' ' '\n'
+}
+
+tmux-window() {
+  tmux new-window -n cheat "$SHELL" -c "$1 | less -R"
+}
+
+utils=("xargs" "find" "sed" "awk" "fd")
+languages=("c" "cpp" "bash" "lua" "rust" "golang" "python" "javascript" "typescript")
+
+all=("${languages[@]}" "${utils[@]}")
+selected=$(list "${all[@]}" | fzf)
+
+read -p "Enter query: " -r query
+
+if grep -qw "$selected" <<< "${all[@]}"
+then
+  tmux-window "curl -s cht.sh/$selected/$(echo "$query" | tr ' ' '+')"
+else
+  tmux-window "curl -s cht.sh/$selected/~$query"
+fi
