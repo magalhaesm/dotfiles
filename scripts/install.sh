fd78e63b9837965c21cc70643f290ab83d4cc7ae
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-02-24 17:38:06 -0300

    build: as dependências devem ser instaladas manualmente

--- scripts/install.sh
+++ scripts/install.sh
@@ -9,9 +9,22 @@ DOTFILES="$HOME/.dotfiles"
 USER_ENVS="\$HOME/.config/zsh/exports.zsh"
 
 dependencies=(
-  stow kitty tmux delta
-  nvim gcc node
-  zsh rg bat zoxide fzf exa fd starship
+  bat
+  delta
+  exa
+  fd
+  fzf
+  gcc
+  kitty
+  node
+  nvim
+  rg
+  starship
+  stow
+  tmux
+  xsel
+  zoxide
+  zsh
 )
 
 missing=()
@@ -20,46 +33,47 @@ info() {
   echo -e "\n${CYAN}info:${NC} $1"
 }
 
+
+erro() {
+  echo -e "\n${RED}ERRO:${NC} $1"
+}
+
 installed() {
-  if [ -x "$(command -v "$1")" ]
-  then
+  if [ -x "$(command -v "$1")" ]; then
     echo -e "${GREEN} ✓${NC} $1"
     return 0
   else
-    echo -e "${RED} ✗${NC} $1 <-- (not installed)"
+    echo -e "${RED} ✗${NC} $1 <-- (não encontrado)"
     return 1
   fi
 }
 
 check_sys_deps() {
-  if ! installed git || ! installed curl
-  then
-    echo -e "\nPlease, install and try again."
+  if ! installed git || ! installed curl; then
+    echo -e "\nPor favor, instale e tente de novo."
     exit 1
   fi
 }
 
 clone_dotfiles() {
-  [ -d "$DOTFILES" ] && { echo "Directory $DOTFILES already exists." && return; }
+  [ -d "$DOTFILES" ] && { echo "Diretório $DOTFILES já existe." && return; }
   git clone https://github.com/magalhaesm/dotfiles.git "$DOTFILES"
 }
 
 check_deps() {
-  for dep in "${dependencies[@]}"
-  do
+  for dep in "${dependencies[@]}"; do
     ! installed "$dep" && missing+=("$dep")
   done
+  [ "${#missing[@]}" -gt 0 ] && { erro "Dependências faltando."; exit 1; }
 }
 
 install_missing() {
-  if [ "${#missing[@]}" != 0 ]
-  then
-    read -p " Install missing dependencies? [y/N] " -r install
+  if [ "${#missing[@]}" != 0 ]; then
+    read -p " Instalar dependências faltando? [y/N] " -r install
     [ "$install" != "y" ] && exit
 
-    info "Installing dependencies..."
-    for tool in "${missing[@]}"
-    do
+    info "Instalando dependências..."
+    for tool in "${missing[@]}"; do
       echo -e " ==> ${GREEN}${tool}${NC}"
       "$DOTFILES/scripts/install-$tool"
     done
@@ -71,59 +85,59 @@ config() {
 }
 
 stow_configs() {
-  pushd "$DOTFILES" &>/dev/null || exit 1
+  pushd "$DOTFILES" &>/dev/null || exit
 
   config git
-  for tool in "${dependencies[@]}"
-  do
+  for tool in "${dependencies[@]}"; do
     config "$tool"
   done
 
-  popd &>/dev/null || exit 1
+  popd &>/dev/null || exit
 }
 
 set_zshenv() {
-  if ! grep -q $USER_ENVS /etc/zsh/zshenv
-  then
-    echo "Editing /etc/zsh/zshenv"
+  local file="/etc/zsh/zshenv"
+  if ! grep -q $USER_ENVS $file; then
+    echo "Editando $file"
 
-    echo -e "\n# Load user envs" | sudo tee -a /etc/zsh/zshenv
-    echo "[ -f $USER_ENVS ] && source $USER_ENVS" | sudo tee -a /etc/zsh/zshenv
-    info "Restart the session to take effect."
+    echo -e "\n# Variáveis de ambiente do usuário" | sudo tee -a $file
+    echo "[ -f $USER_ENVS ] && source $USER_ENVS" | sudo tee -a $file
+    info "Reinicie a sessão para que as modificações tenham efeito."
   fi
 }
 
 set_shell() {
+  local zsh
   zsh=$(command -v zsh)
-  [ "$SHELL" != "$zsh" ] && sudo chsh -s "$zsh" "$USER" || echo "Already configured."
+  [ "$SHELL" != "$zsh" ] && sudo chsh -s "$zsh" "$USER" || echo "Já configurado."
 }
 
 main() {
   echo "---------------------------------------------------------"
-  echo " ==> Installing terminal, tools and configurations "
+  echo " ==> Instalando terminal, ferramentas e configurações"
   echo "---------------------------------------------------------"
 
-  info "Checking installation dependencies..."
+  info "Verificando dependências de instalação..."
   check_sys_deps
 
-  info "Obtaining repository..."
+  info "Obtendo repositório..."
   clone_dotfiles
 
-  info "Checking dependencies..."
+  info "Verificando dependências do ambiente..."
   check_deps
-  install_missing
+  # install_missing
 
-  info "Stowing configuration files..."
+  info "Vinculando arquivos de configuração..."
   stow_configs
 
-  info "Ensuring user environment variables..."
+  info "Configurando variáveis de ambiente..."
   set_zshenv
 
-  info "Installing Oh-my-zsh..."
+  info "Instalando Oh-my-zsh..."
   "$DOTFILES/scripts/install-omz"
 
-  info "Setting zsh as default shell..."
+  info "Configurando zsh como shell padrão..."
   set_shell
 }
 
-main
+main "$@"
