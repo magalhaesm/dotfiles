7afb9d5cd1d61aa143f58992e00dd1930adc4298
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-02-23 21:14:37 -0300

    feat(nvim): add mapping to run tests in lua

--- nvim/.config/nvim/after/ftplugin/lua.lua
+++ nvim/.config/nvim/after/ftplugin/lua.lua
@@ -1,1 +1,12 @@
 vim.opt_local.formatoptions:remove "o"
+
+------------------------------------------------------------------------------
+-- Nvim Dev
+------------------------------------------------------------------------------
+local ok, _ = pcall(require, "mapx")
+if not ok then
+  return
+end
+
+local opts = { buffer = 0, silent = true }
+nnoremap ("<localleader>t", "<Plug>PlenaryTestFile", "Run Test", opts)
