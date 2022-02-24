86f253976a58e5d3821f53344b330d03fe4f3d2a
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-02-24 15:08:29 -0300

    feat(nvim): adiciona funções globais de teste ao sumneko_lua

--- nvim/.config/nvim/lua/mm/lsp/settings/sumneko_lua.lua
+++ nvim/.config/nvim/lua/mm/lsp/settings/sumneko_lua.lua
@@ -5,7 +5,14 @@ return {
         version = "LuaJIT",
       },
       diagnostics = {
-        globals = { "vim" },
+        globals = {
+          "vim",
+          "describe",
+          "it",
+          "assert",
+          "before_each",
+          "after_each",
+        },
       },
       workspace = {
         library = {
