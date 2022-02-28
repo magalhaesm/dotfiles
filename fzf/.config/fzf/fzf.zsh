fd79676a6c82a5bd29070e1f4dfb5392c230d711
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2022-02-28 12:23:17 -0300

    refactor(fzf): altera caminho para carregar auto-completar e mapeamentos

--- fzf/.config/fzf/fzf.zsh
+++ fzf/.config/fzf/fzf.zsh
@@ -6,9 +6,10 @@
 
 # Auto-completion
 # ---------------
-[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh" 2> /dev/null
+# [[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/fzf/completion.zsh" 2> /dev/null
 
 # Key bindings
 # ------------
-[[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/completion.zsh" 2> /dev/null
-# source "/opt/fzf/shell/key-bindings.zsh"
+# [[ $- == *i* ]] && source "/usr/share/doc/fzf/examples/key-bindings.zsh" 2> /dev/null
+[[ $- == *i* ]] && source "/usr/share/fzf/key-bindings.zsh" 2> /dev/null
