a1812bdafa197f32ef188eac8e95eaa70fd88979
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-01-24 19:29:17 -0300

    nvim: atualiza configuração do tema de cores

--- nvim/.config/nvim/lua/mm/colorscheme.lua
+++ nvim/.config/nvim/lua/mm/colorscheme.lua
@@ -1,98 +1,137 @@
 local ok, monokai = pcall(require, "monokai")
-if ok then
-  local palette = monokai.classic
-  monokai.setup {
-    custom_hlgroups = {
-      DiagnosticError = {
-        palette.red,
-      },
-      DiagnosticSignWarn = {
-        fg = palette.orange,
-      },
-      NormalFloat = {
-        bg = palette.base2,
-      },
-      FloatBorder = {
-        fg = palette.border,
-      },
-
-      -- Lsp
-      LspReferenceText = {
-        bg = palette.base4,
-      },
-      LspReferenceRead = {
-        bg = palette.base4,
-      },
-      LspReferenceWrite = {
-        bg = palette.base4,
-      },
-      LspInfoBorder = {
-        fg = palette.border,
-      },
-
-      -- GitSigns
-      GitSignsChange = {
-        fg = palette.yellow,
-        bg = palette.base2,
-      },
-
-      -- Telescope
-      TelescopeNormal = {
-        bg = palette.base2,
-      },
-
-      -- Treesitter
-      TSNote = {
-        fg = palette.aqua,
-        style = "bold",
-      },
-      TSWarning = {
-        fg = palette.orange,
-        style = "bold",
-      },
-      TSDanger = {
-        fg = palette.pink,
-        style = "bold",
-      },
-      TSParameter = {
-        fg = palette.orange,
-        style = "italic",
-      },
-      TSVariableBuiltin = {
-        fg = palette.purple,
-      },
-
-      -- NvimTree
-      PanelHeading = {
-        fg = palette.orange,
-        bg = palette.base0,
-        style = "bold",
-      },
-      NvimTreeNormal = {
-        bg = palette.base0,
-      },
-      NvimTreeVertSplit = {
-        fg = palette.base0,
-        bg = palette.base0,
-      },
-
-      -- IndentBlankline
-      IndentBlanklineContextChar = {
-        fg = palette.base6,
-      },
-
-      -- Toml
-      tomlTable = {
-        fg = palette.green,
-      },
-      tomlKey = {
-        fg = palette.pink,
-      },
-
-      -- Makefile
-      TSSymbol = {
-        fg = palette.pink,
-      },
-    },
-  }
+if not ok then
+  return
 end
+local palette = require('monokai').pro
+local custom_hlgroups = {
+  DiagnosticError = {
+    palette.red,
+  },
+  DiagnosticSignWarn = {
+    fg = palette.orange,
+  },
+  NormalFloat = {
+    bg = palette.base2,
+  },
+  FloatBorder = {
+    fg = palette.border,
+  },
+
+  -- Lsp
+  LspReferenceText = {
+    bg = palette.base4,
+  },
+  LspReferenceRead = {
+    bg = palette.base4,
+  },
+  LspReferenceWrite = {
+    bg = palette.base4,
+  },
+  LspInfoBorder = {
+    fg = palette.border,
+  },
+
+  -- GitSigns
+  GitSignsChange = {
+    fg = palette.yellow,
+    bg = palette.base2,
+  },
+
+  -- Telescope
+  TelescopeNormal = {
+    bg = palette.base2,
+  },
+
+  -- Treesitter
+  ["@include"] = {
+    fg = palette.pink,
+  },
+  ["@text.note"] = {
+    fg = palette.aqua,
+    style = "bold",
+  },
+  ["@text.warning"] = {
+    fg = palette.orange,
+    style = "bold",
+  },
+  ["@text.todo"] = {
+    fg = palette.orange,
+    style = "bold",
+  },
+  ["@text.danger"] = {
+    fg = palette.pink,
+    style = "bold",
+  },
+  ["@parameter"] = {
+    fg = palette.orange,
+    style = "italic",
+  },
+  ["@function.builtin"] = {
+    fg = palette.aqua,
+  },
+  ["@function.call"] = {
+    fg = palette.green,
+    style = "italic",
+  },
+  ["@constructor"] = {
+    fg = palette.aqua,
+  },
+  ["@constant.builtin"] = {
+    fg = palette.purple,
+  },
+  ["@constant"] = {
+    fg = palette.aqua,
+  },
+  ["@string.escape"] = {
+    fg = palette.purple,
+  },
+  ["@storageclass"] = {
+    fg = palette.pink,
+    style = "italic",
+  },
+  ["@preproc"] = {
+    fg = palette.pink,
+  },
+  ["@define"] = {
+    fg = palette.pink,
+  },
+  ["@type.qualifier"] = {
+    fg = palette.pink,
+  },
+
+  -- NvimTree
+  PanelHeading = {
+    fg = palette.orange,
+    bg = palette.base0,
+    style = "bold",
+  },
+  NvimTreeNormal = {
+    bg = palette.base0,
+  },
+  NvimTreeVertSplit = {
+    fg = palette.base0,
+    bg = palette.base0,
+  },
+
+  -- IndentBlankline
+  IndentBlanklineContextChar = {
+    fg = palette.base6,
+  },
+
+  -- Toml
+  tomlTable = {
+    fg = palette.green,
+  },
+  tomlKey = {
+    fg = palette.pink,
+  },
+
+  -- Makefile
+  TSSymbol = {
+    fg = palette.pink,
+  },
+}
+monokai.setup {
+  palette = palette,
+  custom_hlgroups = custom_hlgroups
+}
