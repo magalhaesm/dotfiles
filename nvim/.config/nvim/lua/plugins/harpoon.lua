963ce2c9321f715800ed08822e7f75cd948548d2
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-06-22 19:05:49 -0300

    nvim: config harpoon

--- nvim/.config/nvim/lua/plugins/harpoon.lua
+++ nvim/.config/nvim/lua/plugins/harpoon.lua
@@ -8,13 +8,13 @@ return {
       local mark = require("harpoon.mark")
       local ui = require("harpoon.ui")
 
-      vim.keymap.set("n", "<leader>a", mark.add_file)
-      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu)
+      vim.keymap.set("n", "<leader>a", mark.add_file, { desc = "Add to Harpoon" })
+      vim.keymap.set("n", "<C-e>", ui.toggle_quick_menu, { desc = "Open Harpoon" })
 
-      -- vim.keymap.set("n", "<C-h>", function() ui.nav_file(1) end)
-      -- vim.keymap.set("n", "<C-t>", function() ui.nav_file(2) end)
-      -- vim.keymap.set("n", "<C-n>", function() ui.nav_file(3) end)
-      -- vim.keymap.set("n", "<C-s>", function() ui.nav_file(4) end)
+      vim.keymap.set("n", "<leader>h", function() ui.nav_file(1) end, { desc = "Go to file 1" })
+      vim.keymap.set("n", "<leader>j", function() ui.nav_file(2) end, { desc = "Go to file 2" })
+      vim.keymap.set("n", "<leader>k", function() ui.nav_file(3) end, { desc = "Go to file 3" })
+      vim.keymap.set("n", "<leader>l", function() ui.nav_file(4) end, { desc = "Go to file 4" })
     end
   }
 }
