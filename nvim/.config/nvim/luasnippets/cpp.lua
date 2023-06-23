be1cdec4656b5a3fbddbca381dd3cb4b98377fa6
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2023-06-23 16:54:53 -0300

    nvim: add config to cpp

---
+++ nvim/.config/nvim/luasnippets/cpp.lua
@@ -0,0 +1,21 @@
+local ls = require "luasnip"
+
+local cpp_main = [[
+int	main(int argc, const char **argv)
+{
+	$0
+	return (0);
+}
+]]
+
+local cpp_class = [[
+class $1 {
+public:
+    $0
+};
+]]
+
+ls.add_snippets("cpp", {
+  ls.parser.parse_snippet({ trig = "main", dscr = "Standard main function" }, cpp_main),
+  ls.parser.parse_snippet({ trig = "class" }, cpp_class),
+})
