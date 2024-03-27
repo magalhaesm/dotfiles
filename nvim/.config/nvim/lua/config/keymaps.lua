9441f1b9b9bec68c6ff0542d85d502c7e86d6da9
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2024-03-26 21:00:58 -0300

    nvim: add docs to keymaps

--- nvim/.config/nvim/lua/config/keymaps.lua
+++ nvim/.config/nvim/lua/config/keymaps.lua
@@ -1,66 +1,60 @@
-local util = require("config.util")
 local map = vim.keymap.set
 local opts = { silent = true }
 
-map("n", "<leader>gg", function()
-  util.float_term({ "lazygit" })
-end, { desc = "Lazy[G]it" })
+-- Save and Quit mappings
+map('n', '<leader>w', '<cmd>w!<cr>', { desc = 'Save' })
+map('n', '<leader>q', '<cmd>q!<cr>', { desc = 'Quit' })
 
-map("n", "<leader>w", "<cmd>w!<CR>", { desc = "Save" })
-map("n", "<leader>q", "<cmd>q!<CR>", { desc = "Quit" })
+-- Use CTRL+<hjkl> to switch between windows
+--  See `:help wincmd` for a list of all window commands
+map('n', '<C-h>', '<C-w>h', { desc = 'Move to the left window' })
+map('n', '<C-j>', '<C-w>j', { desc = 'Move to the lower window' })
+map('n', '<C-k>', '<C-w>k', { desc = 'Move to the upper window' })
+map('n', '<C-l>', '<C-w>l', { desc = 'Move to the right window' })
 
-map("n", "<C-h>", "<C-w>h")
-map("n", "<C-j>", "<C-w>j")
-map("n", "<C-k>", "<C-w>k")
-map("n", "<C-l>", "<C-w>l")
+-- Use CTRL+arrows to resize windows
+map('n', '<C-Up>', '<cmd>resize -2<cr>', { desc = 'Decrease height' })
+map('n', '<C-Down>', '<cmd>resize +2<cr>', { desc = 'Increase height' })
+map('n', '<C-Left>', '<cmd>vertical resize -2<cr>', { desc = 'Decrease width' })
+map('n', '<C-Right>', '<cmd>vertical resize +2<cr>', { desc = 'Increase width' })
 
--- Resize windows
-map("n", "<C-Up>", "<cmd>resize -2<CR>")
-map("n", "<C-Down>", "<cmd>resize +2<CR>")
-map("n", "<C-Left>", "<cmd>vertical resize -2<CR>")
-map("n", "<C-Right>", "<cmd>vertical resize +2<CR>")
+-- Use SHIFT+<hl> to move between buffers
+-- map('n', '<S-h>', '<cmd>bprev<cr>', { desc = 'Go to previous buffer' })
+-- map('n', '<S-l>', '<cmd>bnext<cr>', { desc = 'Go to next buffer' })
 
--- Buffer navigation: force
-map("n", "<space><S-h>", "<cmd>bprev<CR>")
-map("n", "<space><S-l>", "<cmd>bnext<CR>")
+-- Jump to beginning and end of line mappings
+map('n', '<A-h>', '^', { desc = 'Move to the beginning of the line' })
+map('n', '<A-l>', '$', { desc = 'Move to the end of the line' })
 
--- Move cursor to the first/last character of the line
-map("n", "<A-h>", "0")
-map("n", "<A-l>", "$")
+-- Use <BS> to turn off the highlight
+map('n', '<BS>', '<cmd>nohlsearch<cr>', { desc = 'Turn off the highlight' })
 
--- Turn off highlight
-map("n", "<BS>", "<cmd>nohlsearch<CR>")
+-- Use 'kj' to exit insert mode
+map('i', 'kj', '<ESC>', { desc = 'Exit insert mode' })
 
--- Exit insert mode
-map("i", "kj", "<ESC>")
+-- Press '>' and '<' to shift lines
+map('x', '>', '>gv', { desc = 'Shift lines to the right' })
+map('x', '<', '<gv', { desc = 'Shift lines to the left' })
 
--- Shift highlighted lines
-map("x", ">", ">gv")
-map("x", "<", "<gv")
-
--- Move lines up and down
-map("n", "<A-j>", "<cmd>m .+1<CR>==")
-map("n", "<A-k>", "<cmd>m .-2<CR>==")
-map("x", "<A-j>", ":move '>+1<CR>gv-gv", opts)
-map("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
+-- Use ALT+<jk> to move lines
+map('n', '<A-j>', '<cmd>m .+1<cr>==')
+map('n', '<A-k>', '<cmd>m .-2<cr>==')
+map('x', '<A-j>', ":move '>+1<cr>gv-gv", opts)
+map('x', '<A-k>', ":move '<-2<cr>gv-gv", opts)
 
 -- Select all
-map("o", "ae", ":normal! ggVG<CR>", opts)
-map("v", "ae", ":normal! ggVG<CR>", opts)
+map('o', 'ae', ':normal! ggVG<cr>', opts)
+map('v', 'ae', ':normal! ggVG<cr>', opts)
 
--- Quickfix
-map("n", "]q", "<cmd>cnext<CR>zz", { desc = "Quickfix: next" })
-map("n", "[q", "<cmd>cprev<CR>zz", { desc = "Quickfix: prev" })
-map("n", "]l", "<cmd>lnext<cr>zz", { desc = "Loclist: next" })
-map("n", "[l", "<cmd>lprev<cr>zz", { desc = "Loclist: prev" })
+-- Quickfix mappings
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Previous [D]iagnostic' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Next [D]iagnostic' })
+vim.keymap.set('n', 'gl', vim.diagnostic.open_float, { desc = '[L]ine Diagnostic' })
+vim.keymap.set('n', '<leader>Q', vim.diagnostic.setloclist, { desc = '[Q]uickfix List' })
 
--- Paste on command line
-map("c", "<C-v>", "<C-r>+")
+-- Paste on command line mapping
+map('c', '<C-v>', '<C-r>+', { desc = 'Paste on command line' })
 
--- Split window
--- map("n", "<leader>ss", "<cmd>split<CR>")
--- map("n", "<leader>sv", "<cmd>vsplit<CR>")
-
-map("v", "<leader>p", '"_dP')
-map("i", "<C-l>", "<Right>")
-map("i", "<C-h>", "<Left>")
+-- Move cursor right and left in insert mode mappings
+map('i', '<C-l>', '<Right>', { desc = 'Move cursor right' })
+map('i', '<C-h>', '<Left>', { desc = 'Move cursor left' })
