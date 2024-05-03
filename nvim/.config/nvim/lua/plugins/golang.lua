91ceac7995fc8ced5d5d70f1cf417a57533918e8
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2024-05-02 22:46:49 -0300

    nvim: golang files load luasnip

--- nvim/.config/nvim/lua/plugins/golang.lua
+++ nvim/.config/nvim/lua/plugins/golang.lua
@@ -6,7 +6,9 @@ return {
     'nvim-treesitter/nvim-treesitter',
   },
   config = function()
-    require('go').setup()
+    require('go').setup({
+      luasnip = true,
+    })
   end,
   event = { 'CmdlineEnter' },
   ft = { 'go', 'gomod' },
