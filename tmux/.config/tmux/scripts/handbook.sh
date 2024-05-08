401fdd067af8da473629357adfb49c654db29bc0
Marcelo Magalhães <marcelomagalhaes@protonmail.com>
2024-05-07 21:16:27 -0300

    tmux: add handbook as submodule

--- tmux/.config/tmux/scripts/handbook.sh
+++ tmux/.config/tmux/scripts/handbook.sh
@@ -5,7 +5,7 @@
 # Script to open files from the handbook directory in a new tmux window,
 # and display file contents using 'bat' and 'less'
 
-HANDBOOK="$(xdg-user-dir DOCUMENTS)/handbook/"
+HANDBOOK="$HOME/.dotfiles/handbook/"
 
 tmux-window() {
   tmux new-window -n "$(basename "$0" .sh)" "$SHELL" -c "$1"
@@ -14,8 +14,8 @@ tmux-window() {
 open_handbook() {
   local selected
 
-  selected=$(find "$HANDBOOK" -maxdepth 1 -type f \
-    ! \( -name "README.md" -o -name "LICENSE" \) |\
+  selected=$(find "$HANDBOOK" -type f \
+    ! -name "LICENSE" ! -name "README.md" ! -name ".git" |\
     fzf -e -i --delimiter '/' --with-nth '-1' --prompt="Handbook: ")
 
   echo "$selected"
